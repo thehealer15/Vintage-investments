5220674ebac976caef9491fb2b79f5d2a823c5c4
Akshay Pawar <akshaygpawar15@gmail.com>
2022-03-28 20:27:42 +0530

    after animations added dark and light mode to feed fragment

--- app/src/main/java/Screens/feed_fragment.java
+++ app/src/main/java/Screens/feed_fragment.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import com.example.app.R;
 
@@ -17,7 +19,7 @@ import com.example.app.R;
 public class feed_fragment extends Fragment {
     private View parentHolder;
     private Context context;
-
+    RadioGroup radioGroup;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -32,6 +34,33 @@ public class feed_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         parentHolder =  inflater.inflate(R.layout.feed_fragment, container, false);
+
+        radioGroup = parentHolder.findViewById(R.id.idRGgroup);
+
+
+        // on below line we are setting on check change method for our radio group.
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // on radio button check change
+                switch (checkedId) {
+                    case R.id.idRBLight:
+                        // on below line we are checking the radio button with id.
+                        // on below line we are setting the text to text view as light mode.
+                        // on below line we are changing the theme to light mode.
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case R.id.idRBDark:
+                        // this method is called when dark radio button is selected
+                        // on below line we are setting dark theme text to our text view.
+                        // on below line we are changing the theme to dark mode.
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
+            }
+        });
+
+
 
         return parentHolder;
  }
