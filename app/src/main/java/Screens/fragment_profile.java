ed9cb1a4391ea1198790a3709204703aef4b86f5
Akshay Pawar <akshaygpawar15@gmail.com>
2022-04-09 22:19:48 +0530

    final commit

--- app/src/main/java/Screens/fragment_profile.java
+++ app/src/main/java/Screens/fragment_profile.java
@@ -2,6 +2,7 @@ package Screens;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -18,7 +19,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.app.R;
+import com.google.firebase.auth.FirebaseAuth;
 
+import Screens.authentication.WelcomeActivity;
 import Screens.profile.AccountDetailsFragament;
 
 
@@ -26,7 +29,7 @@ public class fragment_profile extends Fragment {
     private View parentHolder;
     private Context context;
     private CardView account;
-    private Button allOrders, helpNSupport;
+    private Button allOrders, helpNSupport ,log_out;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -54,6 +57,11 @@ public class fragment_profile extends Fragment {
         account = parentHolder.findViewById(R.id.cardView);
         allOrders = parentHolder.findViewById(R.id.all_orders_btn);
         helpNSupport = parentHolder.findViewById(R.id.help_n_support_btn);
+        log_out = parentHolder.findViewById(R.id.log_out);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            log_out.setVisibility(View.GONE);
+        }
 
         account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +82,14 @@ public class fragment_profile extends Fragment {
                         })
                         .setIcon(android.R.drawable.ic_menu_call)
                         .show();
+            }
+        });
+
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity() , WelcomeActivity.class));
             }
         });
 
